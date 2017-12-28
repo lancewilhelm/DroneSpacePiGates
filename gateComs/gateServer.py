6d1adc7960edf36cbe779db8c4a1a2fa0741596d
Timothy <sky.multigp@gmail.com>
2017-12-28 12:49:05 -0700

    change log level for a sec

--- gateComs/gateServer.py
+++ gateComs/gateServer.py
@@ -5,7 +5,7 @@ import select
 import time
 import DSUtils
 import logging
-logging.basicConfig(filename='/home/pi/DSServer.log',level=logging.INFO)
+logging.basicConfig(filename='/home/pi/DSServer.log',level=logging.DEBUG)
 try:
     import cPickle as pickle
 except:
