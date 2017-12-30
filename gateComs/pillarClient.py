13c5f114263ba2f9dfadd0af635b62f422f6b268
Timothy <sky.multigp@gmail.com>
2017-12-30 01:36:20 -0700

    pillar args

--- gateComs/pillarClient.py
+++ gateComs/pillarClient.py
@@ -1,4 +1,19 @@
 import DSElement
+import sys
+import argparse
+
+#lets allow the user to call this script with options
+parser = argparse.ArgumentParser(description="Starts a gate client")
+parser.add_argument('--h', type=str, help="-h: help\n-l: \n-d: ")
+parser.add_argument('-l', default="off", help="log level (high,medium,low,off)")
+parser.add_argument('-f', default="/home/pi/DSElement.log", help="location to save the log file")
+parser.add_argument('-d', default=0, help="developer mode (1,0)")
+parser.add_argument('-ledCount', default=337, help="the number of leds this device controls (integer)")
+parser.add_argument('-i', default="gatemaster", help="the ip address of the gateServer")
+parser.add_argument('-p', default=13246, help="gateServer port")
+parser.add_argument('-c', default="green", help="the color of the element before we connect to the server")
+args = parser.parse_args()
+#all options have defaults, so we don't HAVE to provide them when calling the script
 
 print("Starting pillar client")
 DSElement.pillar(527).start()
