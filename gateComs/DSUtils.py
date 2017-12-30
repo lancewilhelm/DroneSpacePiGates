eec3e47c77f5bd1b4bd6d9a5288b07bca410f904
Timothy <sky.multigp@gmail.com>
2017-12-29 20:58:01 -0700

    stay rainbow

--- gateComs/DSUtils.py
+++ gateComs/DSUtils.py
@@ -32,7 +32,7 @@ class Gate: #this is our representation of a gate
     def isAlive(self):
         alive = True
         currentTime = self.getTime()
-        if((currentTime-self.lastUpdate) > 30000):
+        if((currentTime-self.lastUpdate) > 60000):
             alive = False
         return alive
 
