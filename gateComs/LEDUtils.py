c17383360f114138aee8e7576e8bf5231f24934f
Timothy <sky.multigp@gmail.com>
2018-01-17 00:43:11 -0700

    fixed indent

--- gateComs/LEDUtils.py
+++ gateComs/LEDUtils.py
@@ -213,9 +213,9 @@ class LEDStrip:
 
       self.strip.show()
 
-  def tempFlash(self):
-      x = self.updateFrame(255)
-      for j in range(self.strip.numPixels()):
-          lum = ((math.sin((x*math.pi*0.02)-(math.pi*.5))+1)*127.5
-          self.strip.setPixelColor(j, Color(lum,lum,lum))
-      self.strip.show()
+    def tempFlash(self):
+        x = self.updateFrame(255)
+        for j in range(self.strip.numPixels()):
+            lum = ((math.sin((x*math.pi*0.02)-(math.pi*.5))+1)*127.5
+            self.strip.setPixelColor(j, Color(lum,lum,lum))
+        self.strip.show()
