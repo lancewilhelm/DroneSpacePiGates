34a57ac12f93035a5e483a447d1f39d5b7b95e9e
Timothy <sky.multigp@gmail.com>
2018-01-17 01:12:10 -0700

    no more casting bs

--- gateComs/LEDUtils.py
+++ gateComs/LEDUtils.py
@@ -217,5 +217,5 @@ class LEDStrip:
         x = self.updateFrame(255)
         for j in range(self.strip.numPixels()):
             brt = int(round((math.sin((x*math.pi*0.02)-(math.pi*.5))+1)*127.5))
-            self.strip.setPixelColor(j, Color((int)brt,(int)brt,(int)brt))
+            self.strip.setPixelColor(j, Color(brt,brt,brt))
         self.strip.show()
