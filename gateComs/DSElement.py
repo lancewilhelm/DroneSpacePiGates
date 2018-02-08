d005a1dc2323ecacdde434793afd58a92c90d01d
Timothy <sky.multigp@gmail.com>
2018-02-07 18:02:53 -0700

    lets make this thing field testable

--- gateComs/DSElement.py
+++ gateComs/DSElement.py
@@ -10,6 +10,10 @@ import traceback
 import argparse
 import psutil
 
+import serial
+import Pilot
+from serial.tools import list_ports
+
 try:
     import LEDUtils
 except ImportError:
@@ -20,6 +24,21 @@ try:
     import cPickle as pickle
 except:
     import pickle
+
+CALIBRATE = 0;    #the moments while a module is discovering its noise floor
+STANDBY = 1;      #the moments before a race starts
+START = 2;        #the moment when the race starts
+FAR = 3;          #the moments while a quad is out of the bubble
+ENTER = 4;        #the moments while quad passes through the bubble
+PASS = 5;         #the moment an rssi peaks inside the bubble
+EXIT = 6;         #the moment when a quad exits the bubble
+CHANNEL_HOP = 9;  #the moments while a module stablizes after a channel change. this should only happen when a quad is out of the bubble
+FINISHED = 10;    #the moment when the race is completed
+
+def sendAnimation(animation):
+    port = 13247
+    ip = ""
+    DSWebClient.sendTempAnimation(ip,port,animation)
 
 class gate:
     def __init__(self,args):
@@ -55,6 +74,8 @@ class element:
         logFile = args.f
         logging.basicConfig(filename=logFile,level=logLevel)
 
+
+
         self.serverAddress = args.i
         self.port = args.p
         self.currentColor = args.c
@@ -64,10 +85,76 @@ class element:
         self.keepaliveDelay = 5000 #keepalive delay in ms
         self.connectionTimeout = 15000 #time in ms of not being able to send before we consider the network disconnected
         self.tempAnimationQueue = []
+        self.serial = None
+        self.pilots = []
+
+        self.connectArduino()
+
+    def getSendableLaps(self):
+        laps = []
+        for pilot in self.pilots:
+            for lap in pilot.laps:
+                laps.append([pilot.name,lap.duration,lap.number])
+        return laps
+
+    def clearLaps(self):
+        self.disconnectArduino()
+        self.connectArduino
+
+    def connectArduino(self):
+        try:
+            #arduinoCom = next(list_ports.grep("rduino"))
+
+            arduinoCom = "/dev/ttyUSB0"
+            #arduinoCom = "/dev/ttyACM0"
+            #arduinoCom = "COM11"
+            #print("arduino port: "+str(arduinoCom.device))
+            #ser = serial.Serial(str(arduinoCom.device))  # open serial port
+            self.serial = serial.Serial(arduinoCom,115200)
+            #print(ser.name)         # check which port was really used
+            self.pilots = []
+            self.pilots.append(Pilot.pilot("Sky",0,"bluebang"))
+            self.pilots.append(Pilot.pilot("ScraggleFPV",1,"greenbang"))
+            self.pilots.append(Pilot.pilot("RandoBando",2,"flashbang"))
+            self.pilots.append(Pilot.pilot("Ninja",3,"redbang"))
+            return True
+        except Exception as e:
+            logging.debug("failed to connect to arduino ")
+            logging.debug(traceback.format_exc())
+            return False
+
+    def disconnectArduino(self):
+        try:
+            self.serial.close()
+        except Exception as e:
+            print(e)
+
+    def readSerial(self):
+        try:
+            line = self.serial.readline()
+            event = eval(line)
+            #print(event)
+            pilotId = event[0]
+            pilot = self.pilots[pilotId]
+            state = event[1]
+            timestamp = event[2]
+            if(state==PASS):
+                pilot.addLap(0,timestamp)
+                print(str(pilot.name)+": "+str(timestamp))
+                logging.debug(str(pilot.name)+": "+str(timestamp))
+            if(state==ENTER):
+                self.tempAnimationQueue.append(body)
+            if(state==CALIBRATE):
+                print("calibrating module "+str(pilotId))
+            if(state==STANDBY):
+                print("module "+str(pilotId)+" ready")
+        except Exception as e:
+            print(traceback.format_exc())
+            print("bad data: "+str(line))
 
     def createSocket(self,port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind(('',port))
+        sock.bind(('',13248))
         sock.setblocking(0)
         return sock
 
@@ -84,7 +171,7 @@ class element:
             self.updateAnimations(LED)
             data,address = self.recvData(sock)
             if(data):
-                connectionSuccess = self.handleMessage(sock,data,LED)
+                connectionSuccess = self.handleMessage(sock,address,data,LED)
                 break
             else:
                 connectionSuccess = False
@@ -96,7 +183,7 @@ class element:
         else:
             logging.debug("no response from server")
             #lets run our fallback animation
-            self.handleMessage(sock,{"body":self.currentColor,"subject":"updateAnimation","recipient":""},LED)
+            self.handleMessage(sock,address,{"body":self.currentColor,"subject":"updateAnimation","recipient":""},LED)
             self.updateAnimations(LED) #make the animation actually play momentarily
         return connectionSuccess
     def recvData(self,sock): #this is where we handle all recieved data
@@ -225,12 +312,13 @@ class element:
 
             while(True):
                 time.sleep(0.02)
+                self.readSerial()
                 newUpdate = False
                 if self.keepAlive(sock): #let's let the server know we're still there
                     data,address = self.recvData(sock)
                     self.updateAnimations(LED)
                     if(data):
-                        if(self.handleMessage(sock,data,LED)): #if this returns false, we've been disconnected
+                        if(self.handleMessage(sock,address,data,LED)): #if this returns false, we've been disconnected
                             pass
                         else:
                             break
@@ -275,7 +363,7 @@ class element:
             except Exception as e:
                 logging.debug(traceback.format_exc())
 
-    def handleMessage(self,sock,data,LED):
+    def handleMessage(self,sock,address,data,LED):
         if(data):
             try:
                 subject = data['subject'] #the subject of the message ()
@@ -285,6 +373,15 @@ class element:
                 if(subject == "disconnect"):
                     logging.debug("we recieved a disconnect request")
                     return False; #we gotta bail out
+
+                if(subject == "getLapList"):
+                    logging.debug("we recieved a lap list request")
+                    self.sendData(sock,address,"getLapList",self.getSendableLaps(),"")
+
+                if(subject == "clearLapList"):
+                    logging.debug("we recieved a clear lap list request")
+                    self.clearLaps()
+
                 if(subject == "updateColor"):
                     self.currentColor = body
                     logging.debug("updating color: "+str(self.currentColor))
