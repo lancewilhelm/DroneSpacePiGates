9d9038f2d7070114707c950e50a66dfb3a1667d9
Timothy <sky.multigp@gmail.com>
2018-02-10 16:30:04 -0700

    trying to get basic timing test working

--- webapp/timing.py
+++ webapp/timing.py
@@ -45,22 +45,23 @@ def main():
         while(True):
             try:
                 line = ser.readline()
-                event = eval(line)
+                print(line)
+                #event = eval(line)
                 #print(event)
-                pilotId = event[0]
-                pilot = pilots[pilotId]
-                state = event[1]
-                timestamp = event[2]
-                if(state==PASS):
-                    pilot.addLap(0,timestamp)
-                    print(str(pilot.name)+": "+str(timestamp))
-                    logging.debug(str(pilot.name)+": "+str(timestamp))
-                if(state==ENTER):
-                    sendAnimation(pilot.getAnimation())
-                if(state==CALIBRATE):
-                    print("calibrating module "+str(pilotId))
-                if(state==STANDBY):
-                    print("module "+str(pilotId)+" ready")
+                #pilotId = event[0]
+                #pilot = pilots[pilotId]
+                #state = event[1]
+                #timestamp = event[2]
+                #if(state==PASS):
+                #    pilot.addLap(0,timestamp)
+                #    print(str(pilot.name)+": "+str(timestamp))
+                #    logging.debug(str(pilot.name)+": "+str(timestamp))
+                #if(state==ENTER):
+                #    sendAnimation(pilot.getAnimation())
+                #if(state==CALIBRATE):
+                #    print("calibrating module "+str(pilotId))
+                #if(state==STANDBY):
+                #    print("module "+str(pilotId)+" ready")
             except Exception as e:
                 print(traceback.format_exc())
                 print("bad data: "+str(line))
