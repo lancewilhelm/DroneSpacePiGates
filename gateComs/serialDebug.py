cad609ab1a24f7b5111e0d5953da0bae17dd86bd
System Administrator <sky.multigp@gmail.com>
2018-04-13 14:41:04 -0600

    created serial debug tool

---
+++ gateComs/serialDebug.py
@@ -0,0 +1,52 @@
+class Arduino:
+    def __init__(self):
+        self.serial = None
+        self.connected = False
+    def connect(self):
+        print("connecting arduino")
+        try:
+            #arduinoCom = next(list_ports.grep("rduino"))
+
+            arduinoCom = "/dev/ttyUSB0"
+            #arduinoCom = "/dev/ttyACM0"
+            #arduinoCom = "COM11"
+            #print("arduino port: "+str(arduinoCom.device))
+            #ser = serial.Serial(str(arduinoCom.device))  # open serial port
+            self.serial = serial.Serial(arduinoCom,115200, timeout=0.02)
+            #print(ser.name)         # check which port was really used
+            self.clearPilotData()
+            print("arduino connected")
+            return True
+        except Exception as e:
+            logging.debug("failed to connect to arduino ")
+            logging.debug(traceback.format_exc())
+            print("failed to connect arduino")
+            print(e)
+            return False
+
+    def disconnect(self):
+        print("disconnecting arduino")
+        try:
+            self.serial.close()
+        except Exception as e:
+            print(e)
+        self.connected = False
+
+    def printSerial(self):
+        try:
+            if self.connected:
+                line = self.serial.readline()
+                print(line)
+        except Exception as e:
+            print(traceback.format_exc())
+
+    def isConnected(self):
+        return self.connected
+
+def main():
+    arduino = Arduino()
+    arduino.connect()
+    while(arduino.isConnected()):
+        arduino.printSerial()
+    arduino.disconnect()
+    print("program done")
