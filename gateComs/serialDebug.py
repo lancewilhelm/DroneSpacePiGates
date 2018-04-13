1dff7910e2091f79629240c82f27ec48bccf1291
Timothy <sky.multigp@gmail.com>
2018-04-13 15:36:32 -0600

    try to connect to lap timer on multiple ports

--- gateComs/serialDebug.py
+++ gateComs/serialDebug.py
@@ -7,26 +7,33 @@ class Arduino:
     def __init__(self):
         self.serial = None
         self.connected = False
-    def connect(self):
+    def connectArduino(self):
         print("connecting arduino")
-        try:
-            #arduinoCom = next(list_ports.grep("rduino"))
+        arduinoPorts = ["/dev/ttyUSB0","/dev/ttyACM0"]
+        connected = False
+        for arduinoCom in arduinoPorts:
+            try:
+                #arduinoCom = next(list_ports.grep("rduino"))
 
-            arduinoCom = "/dev/ttyACM0"#"/dev/ttyUSB0"
-            #arduinoCom = "/dev/ttyACM0"
-            #arduinoCom = "COM11"
-            #print("arduino port: "+str(arduinoCom.device))
-            #ser = serial.Serial(str(arduinoCom.device))  # open serial port
-            self.serial = serial.Serial(arduinoCom,115200, timeout=0.02)
-            #print(ser.name)         # check which port was really used
-            print("arduino connected")
-            self.connected = True
-        except Exception as e:
-            print("failed to connect to arduino ")
-            print(traceback.format_exc())
-            print("failed to connect arduino")
-            print(e)
-            self.connected = False
+                arduinoCom = "/dev/ttyUSB0"
+                #arduinoCom = "/dev/ttyACM0"
+                #arduinoCom = "COM11"
+                #print("arduino port: "+str(arduinoCom.device))
+                #ser = serial.Serial(str(arduinoCom.device))  # open serial port
+                self.serial = serial.Serial(arduinoCom,115200, timeout=0.02)
+                #print(ser.name)         # check which port was really used
+                self.clearPilotData()
+                print("arduino connected")
+                self.connected = True
+                break
+
+            except Exception as e:
+                logging.debug("failed to connect to arduino on port "+arduinoCom)
+                logging.debug(traceback.format_exc())
+                print("failed to connect arduino")
+                print(e)
+                self.connected = False
+        return connected
 
     def disconnect(self):
         print("disconnecting arduino")
