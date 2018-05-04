726f23b372440168f934b857e764611dd3286e95
bixler13 <bixler.brett@gmail.com>
2018-05-04 13:03:23 -0600

    added Multislash

--- gateComs/DSElement.py
+++ gateComs/DSElement.py
@@ -369,6 +369,8 @@ class element:
                         LED.rainbow()
                     elif(self.currentColor=="pacman"):
                         LED.pacman()
+                    elif(self.currentColor=="multislash")
+                        LED.multislash()
                     else: #it must be a list of rgb values
                         try:
                             LED.customColor(self.currentColor)
